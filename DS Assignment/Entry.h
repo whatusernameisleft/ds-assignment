b59be415d2a7d47a97d6b35eaaf35ef930d4f041
whatusernameisleft <37537195+whatusernameisleft@users.noreply.github.com>
2023-06-27 19:56:44 +0800

    implemented entry class

--- DS Assignment/Entry.h
+++ DS Assignment/Entry.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include <list>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -9,6 +12,7 @@ class Entry {
 	string name;
 	int year;
 	int rent;
+	string region;
 	string location;
 	string type;
 	int rooms;
@@ -16,6 +20,83 @@ class Entry {
 	int bathrooms;
 	int size;
 	string furnished;
-	list<string> facilities;
-	list<string> additionalFacilities;
+	string facilities;
+	string additionalFacilities;
+
+	vector<string> separateSections(string line, char delim) {
+		stringstream s(line);
+		string section;
+		vector<string> sections;
+
+		while (getline(s, section, delim)) {
+			sections.push_back(section);
+		}
+
+		return sections;
+	}
+
+	bool hasChar(string row, char c) {
+		return row.find(c) != string::npos;
+	}
+
+	vector<string> removeCommas(vector<string> strings) {
+		vector<string> newStrings;
+		for (string s : strings) {
+			if (s == ",") continue;
+			else if (s[0] == ',') {
+				s.erase(0, 1);
+			} else if (s.back() == ',') {
+				s.pop_back();
+			}
+			newStrings.push_back(s);
+		}
+
+		return newStrings;
+	}
+
+	vector<string> getColumns(string row) {
+		vector<string> columns;
+		vector<string> quotedSections;
+		if (hasChar(row, '"')) {
+			quotedSections = separateSections(row, '"');
+			quotedSections = removeCommas(quotedSections);
+		}
+
+		if (quotedSections.empty()) columns = separateSections(row, ',');
+		else {
+			columns = separateSections(quotedSections[0], ',');
+			switch (quotedSections.size()) {
+			case 3:
+				if (hasChar(quotedSections[2], ',')) {
+					columns.push_back(quotedSections[1]);
+					vector<string> lastCol = separateSections(quotedSections[2], ',');
+					columns.insert(columns.end(), lastCol.begin(), lastCol.end());
+				} else {
+					columns.push_back(quotedSections[1]);
+					columns.push_back(quotedSections[2]);
+				}
+				break;
+			case 4:
+				quotedSections.erase(quotedSections.begin());
+				columns.insert(columns.end(), quotedSections.begin(), quotedSections.end());
+				break;
+			}
+		}
+
+		return columns;
+	}
+
+	void initEntry(string row) {
+		vector<string> columns = getColumns(row);
+
+		for (string col : columns) {
+			cout << col << endl;
+		}
+	}
+
+	public:
+		Entry(string row) {
+			initEntry(row);
+		}
+
 };
--- DS Assignment/Source.cpp
+++ DS Assignment/Source.cpp
@@ -2,5 +2,7 @@
 #include "Entry.h"
 
 int main() {
+	/*Entry e("100323185,The Hipster @ Taman Desa,2022,RM 4 200 per month,Kuala Lumpur - Taman Desa,Condominium,5,2,6,1842 sq.ft.,Fully Furnished,\"Minimart, Gymnasium, Security, Playground, Swimming Pool, Parking, Lift, Barbeque area, Multipurpose hall, Jogging Track\",\"Air - Cond, Cooking Allowed, Washing Machine\",Kuala Lumpur");
+	Entry e2("100323128,Pangsapuri Teratak Muhibbah 2,,RM 1 000 per month,Kuala Lumpur - Taman Desa,Apartment,3,,2,650 sq.ft.,Fully Furnished,\"Minimart, Jogging Track, Lift, Swimming Pool\",,Kuala Lumpur");*/
 
 }
